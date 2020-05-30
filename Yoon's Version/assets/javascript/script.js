190dd7cd72d86b8a77108cc816f1f117568c1f62
Yoon Ho Ryoo <ryoo94@gmail.com>
2020-05-29 22:54:56 -0700

    minor cleaning up of code

--- Yoon's Version/assets/javascript/script.js
+++ Yoon's Version/assets/javascript/script.js
@@ -15,53 +15,44 @@ var config = {
  var database = firebase.database();
 
 
-//TIMER
+// TIMER
 $(".startBtn").on("click", start);
 $(".pauseBtn").on("click", stop);
 $(".stopBtn").on("click", recordSession);
 $(".stopBtn").on("click", stop);
 $(".stopBtn").on("click", reset);
 
-
+// Variable that will hold our setInterval that runs the stopwatch
 var intervalId;
 
+// This prevents the clock from being sped up
 var clockRunning = false;
 var time = 0;
 var lap = 1;
 
 function start() {
-
-  // DONE: Use setInterval to start the count here and set the clock to running.
+  // Use setInterval to start the count here and set the clock to running.
   if (!clockRunning) {
     intervalId = setInterval(count, 1000);
     clockRunning = true;
   }
 }
 
-
-
 function stop() {
-
-  // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+  // Use clearInterval to stop the count here and set the clock to not be running.
   clearInterval(intervalId);
   clockRunning = false;    
 }
 
-
+// Resets the display to 00:00
 function reset() {
-
   time = 0;
   lap = 1;
 
-  // DONE: Change the "display" div to "00:00."
   $("#display").text("00:00");
-
 }
 
 function recordSession() {
-
-  // DONE: Get the current time, pass that into the timeConverter function,
-  //       and save the result in a variable.
   var converted = timeConverter(time);
   
   database.ref().push({
@@ -70,10 +61,11 @@ function recordSession() {
   });
   
 
-  // DONE: Add the current lap and time to the "laps" div.
+  //Creates an appended list of the CURRENT sessions
+  //If the page is refreshed, the list will be cleared
   $("#lastSession").append("<p>Session: " + lap + " : " + converted + "</p>");
 
-  // DONE: Increment lap by 1. Remember, we can't use "this" here.
+  //Increment lap by 1. Remember, we can't use "this" here.
   lap++;
 
 }
@@ -88,21 +80,18 @@ database.ref().on("child_added", function(snapshot) {
 
 
 function count() {
-
-  // DONE: increment time by 1, remember we cant use "this" here.
+  // increment time by 1
   time++;
 
-  // DONE: Get the current time, pass that into the timeConverter function,
-  //       and save the result in a variable.
+  // Get the current time, pass that into the timeConverter function and save the result in a variable.
   var converted = timeConverter(time);
 
-
-  // DONE: Use the variable we just created to show the converted time in the "display" div.
+  // Use the variable we just created to show the converted time in the "display" div.
   $("#display").text(converted);
 }
 
+// Basically converts the time to correctly show onto the "timer" display
 function timeConverter(t) {
-
   var minutes = Math.floor(t / 60);
   var seconds = t - (minutes * 60);
 
@@ -121,13 +110,9 @@ function timeConverter(t) {
 }
 //END OF TIMER
 
-// TABLE
-
-
 
 
 // Random Quotes Starts Here
-
 function quote() {
   $.ajax({
     url: "https://api.forismatic.com/api/1.0/",
@@ -148,7 +133,6 @@ $("#quoteButton").on("click", function() {
   $("#home").css("height", "725px");
   quote();
 });
-
 // Random quotes Ends Here
 
 // music api
